663be6c0a54c3d1822cefd138b8564966aae8115
ghill <gareth.hill@cesa.catholic.edu.au>
2016-06-09 12:47:15 +0930

    More complete removal.

--- SophosRemoval.ps1
+++ SophosRemoval.ps1
@@ -1,4 +1,4 @@
-# v1.0
+# v1.1
 # Developed by Gareth Hill 
 # Nukes Sophos
 
@@ -7,15 +7,26 @@ $ErrorActionPreference = 'SilentlyContinue'
 foreach ($svc in Get-Service -DisplayName Sophos*) {
     Stop-Service -displayname $svc.DisplayName
 }
-if (Test-Path 'C:\ProgramData\Sophos\Sophos Anti-Virus\Config\machine.xml') {
-       [xml]$sophosConfig = Get-Content 'C:\ProgramData\Sophos\Sophos Anti-Virus\Config\machine.xml'
-       if (($sophosConfig.configuration.components.TamperProtectionManagement.settings.enabled) -And ($sophosConfig.configuration.components.TamperProtectionManagement.settings.enabled -eq "true")) {
-          $sophosConfig.configuration.components.TamperProtectionManagement.settings.enabled = 'false'
-          $sophosConfig.Save('C:\ProgramData\Sophos\Sophos Anti-Virus\Config\machine.xml')
-          Start-Service -displayname "Sophos Anti-Virus" -Wait
-          Stop-Service -displayname "Sophos Anti-Virus"
-       }
-}
+Stop-Process -processname ALMon -force
+Stop-Process -processname ManagementAgentNT -force
+Stop-Process -processname SavService -force
+Stop-Process -processname SAVAdminService -force
+Stop-Process -processname Alsvc -force
+Stop-Process -processname CertificationManagerServiceNT -force
+Stop-Process -processname Sophos.FrontEnd.Service -force
+Stop-Process -processname MgntSvc -force
+Stop-Process -processname RouterNT -force
+Stop-Process -processname PatchEndpointCommunicator -force
+Stop-Process -processname PatchEndpointOrchestrator -force
+Stop-Process -processname PatchServerCommunicator -force
+Stop-Process -processname ssp -force
+Stop-Process -processname SUMService -force
+Stop-Process -processname swc_service -force
+Stop-Process -processname swi_filter -force
+Stop-Process -processname swi_service -force
+Stop-Process -processname SntpService -force
+Stop-Process -processname EnterpriseConsole -force
+Stop-Process -processname Sophos.Messenger -force
 foreach ($sophos32product in (Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -like "Sophos*"})) {
     if ($sophos32product -ne $null) {
         $UninstallGUID = $sophos32product.PSChildName
@@ -28,3 +39,11 @@ foreach ($sophos64product in (Get-ChildItem HKLM:\Software\WOW6432Node\Microsoft
         Start-Process -FilePath msiexec -ArgumentList @("/uninstall $UninstallGUID", "/quiet", "/norestart") -Wait
     }
 }
+foreach ($svc in Get-Service -DisplayName Sophos*) {
+    sc.exe delete $svc.Name
+}
+Remove-Item "C:\Program Files (x86)\Sophos" -Force  -Recurse -ErrorAction SilentlyContinue
+Remove-Item "C:\Program Files (x86)\Common Files\Sophos" -Force  -Recurse -ErrorAction SilentlyContinue
+Remove-Item "C:\Program Files\Sophos" -Force  -Recurse -ErrorAction SilentlyContinue
+Remove-Item "C:\Program Files\Common Files\Sophos" -Force  -Recurse -ErrorAction SilentlyContinue
+Remove-Item "C:\ProgramData\Sophos" -Force  -Recurse -ErrorAction SilentlyContinue
